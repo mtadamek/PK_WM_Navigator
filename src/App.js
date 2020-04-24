5cfeb0fbefd71b12e913b931e9c4a0ae93bf1e3e
Ola <atvolley@gmail.com>
2020-04-24 14:12:12 +0200

    Dodano obsługę przyznawania uprawnień do lokalizacji urządzenia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React, { Component } from 'react'
-import {
-    Text,
-    View,
-} from 'react-native';
-import { connect } from 'react-redux'
-
+import React, {Component} from 'react';
+import {Text, View, Alert, BackHandler} from 'react-native';
+import {connect} from 'react-redux';
+import {request, PERMISSIONS} from 'react-native-permissions';
+ 
 export class App extends Component {
-    render() {
-        return (
-            <View><Text>PK WM Navigator</Text></View>
-        )
-    }
+  locationPermissionDeniedAlert = () => {
+    Alert.alert(
+      'Odmowa dostępu do lokalizacji!',
+      'Aby aplikacja działała poprawnie, konieczny jest dostęp do usług lokalizacyjnych Twojego telefonu.',
+      [
+        {
+          text: 'Anuluj',
+          onPress: () => BackHandler.exitApp(),
+          style: 'cancel',
+        },
+        {text: 'Przyznaj', onPress: () => this.locationPermissionRequest()},
+      ],
+      {cancelable: false},
+    );
+  };
+ 
+  locationPermissionRequest = async () => {
+    try {
+      const result = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      console.log(`PERMISSIONS ${result}`);
+      switch (result) {
+        case 'granted':
+          console.log('Przyznano uprawnienia!');
+          break;
+        case 'denied':
+          console.log('Odmowa uprawinień!');
+          this.locationPermissionDeniedAlert();
+          break;
+ 
+        default:
+          console.log('Brak uprawnień!');
+          break;
+      }
+    } catch (error) {}
+  };
+ 
+  componentDidMount() {
+    this.locationPermissionRequest();
+  }
+ 
+  render() {
+    return (
+      <View>
+        <Text>PK WM Navigator</Text>
+      </View>
+    );
+  }
 }
-
-const mapStateToProps = (state) => ({
-
-})
-
-const mapDispatchToProps = {
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+ 
+const mapStateToProps = state => ({
+  eddystones: state.beacons.eddystones,
+});
+ 
+const mapDispatchToProps = {};
+ 
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
+ 
